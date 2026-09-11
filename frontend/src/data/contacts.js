385ed65258c0512cb3d86868d89bac05e28ebfc6
Jony <valijonodinaev7@gmail.com>
2026-09-11 09:59:25 +0500

    Update contacts.js

--- frontend/src/data/contacts.js
+++ frontend/src/data/contacts.js
@@ -6,7 +6,7 @@ export const contacts = {
   socials: {
     instagram: "https://www.instagram.com/to_akademie",
     telegram: "https://t.me/TOJIKON_OLMONN",
-    whatsapp: "https://wa.me/992000000000", 
+    whatsapp: "https://wa.me/", 
   },
   mapEmbed:
     "https://www.openstreetmap.org/export/embed.html?bbox=68.8054%2C38.5600%2C68.8154%2C38.5680&layer=mapnik&marker=38.564021%2C68.810385",
